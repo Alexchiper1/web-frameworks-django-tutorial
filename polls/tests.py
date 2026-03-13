ec95914d2661a4f6779f298279b5fdfe571571e9
Alexchiper1 <alexchiper20@gmail.com>
2026-03-13 11:45:47 +0000

    Testing

--- polls/tests.py
+++ polls/tests.py
@@ -1,94 +1,78 @@
 import datetime
 
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase, TransactionTestCase, LiveServerTestCase
+from django.urls import reverse, resolve
 from django.utils import timezone
-from django.urls import reverse
+from urllib.request import urlopen
 
-from .models import Question
-
-
-class QuestionModelTests(TestCase):
-    def test_was_published_recently_with_old_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is older than 1 day.
-        """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), False)
+from .models import Question, Choice
+from .views import vote
 
 
-    def test_was_published_recently_with_recent_question(self):
-        """
-        was_published_recently() returns True for questions whose pub_date
-        is within the last day.
-        """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+class PollsSimpleTests(SimpleTestCase):
+    def test_vote_url_resolves_to_vote_view(self):
+        url = reverse("polls:vote", args=[123])
+        match = resolve(url)
+
+        self.assertEqual(url, "/polls/123/vote/")
+        self.assertEqual(match.func, vote)
 
 
-def create_question(question_text, days):
-    """
-    Create a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+class PollsVoteTests(TestCase):
+    def make_question(self):
+        return Question.objects.create(
+            question_text="Favourite language?",
+            pub_date=timezone.now() - datetime.timedelta(days=1)
+        )
 
+    def test_vote_increments_selected_choice_and_redirects(self):
+        question = self.make_question()
 
-class QuestionIndexViewTests(TestCase):
-    def test_no_questions(self):
-        """
-        If no questions exist, an appropriate message is displayed.
-        """
-        response = self.client.get(reverse("polls:index"))
+        choice1 = Choice.objects.create(question=question, choice_text="Python", votes=0)
+        Choice.objects.create(question=question, choice_text="Java", votes=0)
+
+        response = self.client.post(
+            reverse("polls:vote", args=[question.id]),
+            {"choice": choice1.id}
+        )
+        choice1.refresh_from_db()
+
+        self.assertEqual(choice1.votes, 1)
+        self.assertRedirects(response, reverse("polls:results", args=[question.id]))
+
+    def test_vote_without_selecting_choice_shows_error_message(self):
+        question = self.make_question()
+
+        Choice.objects.create(question=question, choice_text="Python", votes=0)
+        response = self.client.post(reverse("polls:vote", args=[question.id]), {})
+
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+        self.assertContains(response, "You didn&#x27;t select a choice.")
 
-    def test_future_question(self):
-        """
-        The detail view of a question with a pub_date in the future
-        returns a 404 not found.
-        """
-        future_question = create_question(question_text="Future question.", days=5)
-        url = reverse("polls:detail", args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
 
-    def test_past_question(self):
-        """
-        The detail view of a question with a pub_date in the past
-        displays the question's text.
-        """
-        past_question = create_question(question_text="Past Question.", days=-5)
-        url = reverse("polls:detail", args=(past_question.id,))
-        response = self.client.get(url)
-        self.assertContains(response, past_question.question_text)
+class PollsTransactionTests(TransactionTestCase):
+    def test_deleting_question_deletes_related_choices(self):
+        question = Question.objects.create(
+            question_text="Delete test",
+            pub_date=timezone.now()
+        )
+        Choice.objects.create(question=question, choice_text="A", votes=0)
+        Choice.objects.create(question=question, choice_text="B", votes=0)
 
-    def test_future_question_and_past_question(self):
-        """
-        Even if both past and future questions exist, only past questions
-        are displayed.
-        """
-        question = create_question(question_text="Past question.", days=-30)
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
-            response.context["latest_question_list"],
-            [question],
+        self.assertEqual(Choice.objects.count(), 2)
+        question.delete()
+
+        self.assertEqual(Choice.objects.count(), 0)
+
+
+class PollsLiveServerTests(LiveServerTestCase):
+    def test_index_page_displays_question_text(self):
+        Question.objects.create(
+            question_text="Live server poll question",
+            pub_date=timezone.now() - datetime.timedelta(hours=1)
         )
 
-    def test_two_past_questions(self):
-        """
-        The questions index page may display multiple questions.
-        """
-        question1 = create_question(question_text="Past question 1.", days=-30)
-        question2 = create_question(question_text="Past question 2.", days=-5)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
-            response.context["latest_question_list"],
-            [question2, question1],
-        )
+        response = urlopen(self.live_server_url + reverse("polls:index"))
+        html = response.read().decode("utf-8")
+
+        self.assertIn("Live server poll question", html)
